85a59ab09d6ac2b52a37828e33f84dd98b176d81
Rang0829 <range940829@gmail.com>
2024-02-23 02:15:37 +0900

    Spring 240222

--- Spring_2402/src/main/java/com/rangrang/a/RegisterController.java
+++ Spring_2402/src/main/java/com/rangrang/a/RegisterController.java
@@ -12,12 +12,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RegisterController {
-//	@RequestMapping(value="/register/save", method= {RequestMethod.GET, RequestMethod.Post})
-//	@RequestMapping("/register/add")   // 신규 회원 가입 화면
-//	@GetMapping("/register/add")
-//	public String register() {
-//		return "registerForm"; // WEB-INF/views/registerForm.jsp
-//	}
+	@RequestMapping(value="/register/add", method= {RequestMethod.GET, RequestMethod.POST})
+	public String register() {
+		return "registerForm"; // WEB-INF/views/registerForm.jsp
+	}
 	
 //	@RequestMapping(value="/register/save", method=RequestMethod.POST)
 	@PostMapping("/register/save")   // 스프링 4.3버전 부터 가능
@@ -27,7 +25,7 @@ public class RegisterController {
 			String msg = URLEncoder.encode("id를 잘못 입력하셨습니다.", "utf-8");
 			
 			m.addAttribute("msg", msg);   // model에 담아서 넘긴다.
-			return "redirect:/register/add";
+			return "forward:/register/add";
 			
 //			return "redirect:/register/add?msg=" + msg;   // URL 재작성 (rewriting) - URL을 고쳐서 /register/add에게 GET 방식으로 URL을 전송하는 것.
 		}
@@ -37,6 +35,6 @@ public class RegisterController {
 	}
 
 	private boolean isVaild(User user) {
-		return true;
-}
+		return false;
+	}
 }
