6cf329cb0dc7766d2a19a856b096818664d3171f
Rang0829 <range940829@gmail.com>
2024-04-05 00:41:30 +0900

    Spring 240404

--- Spring_2403_2/src/main/java/com/rangrang/c/controller/SimpleRestController.java
+++ Spring_2403_2/src/main/java/com/rangrang/c/controller/SimpleRestController.java
@@ -4,16 +4,26 @@ import com.rangrang.c.domain.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class SimpleRestController {
-    @GetMapping("/ajax")
-    public String ajax() {
-        return "ajax";
+//    @GetMapping("/ajax")
+//    public String ajax() {
+//        return "ajax";
+//    }
+
+    @PostMapping("/send")
+//    @ResponseBody
+    public Person test(@RequestBody Person p) {
+        System.out.println("p = " + p);
+        p.setName("ABC");
+        p.setAge(p.getAge() + 10);
+
+        return p;
     }
 
     @PostMapping("/send")
-    @ResponseBody
-    public Person test(@RequestBody Person p) {
+//    @ResponseBody
+    public Person test2(@RequestBody Person p) {
         System.out.println("p = " + p);
         p.setName("ABC");
         p.setAge(p.getAge() + 10);
